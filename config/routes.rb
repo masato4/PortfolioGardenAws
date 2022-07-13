da541bbabd0c6190512428f3365efede59439c97
github masato4 <yags6bok2@gmail.com>
2022-07-13 14:02:23 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'hello/index' => 'hello#index'
   root to:'hello#index'
+  get 'hello/index' => 'hello#index', as: 'hello'
   get 'posts' => 'posts#index'
   get 'posts/new' => 'posts#new'
 end
