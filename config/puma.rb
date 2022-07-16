1c409aa4a83279e1a10376fe86969771a11de8d5
github masato4 <yags6bok2@gmail.com>
2022-07-16 15:16:40 +0900

    first commit

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,10 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+#port ENV.fetch("PORT") { 3000 }
+tmp_path = "/var/www/rails-infra-tutorial/tmp"
+bind "unix://#{tmp_path}/sockets/puma.sock"
+
 
 # Specifies the `environment` that Puma will run in.
 #
